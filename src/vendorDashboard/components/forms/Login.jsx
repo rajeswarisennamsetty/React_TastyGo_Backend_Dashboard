3f0f1a3cea3829aae68499fefa8ba65d7f85ee31
rajeswarisennamsetty <rajeswarisennamsetty@gmail.com>
2025-03-11 00:23:55 +0530

    changes in login

--- src/vendorDashboard/components/forms/Login.jsx
+++ src/vendorDashboard/components/forms/Login.jsx
@@ -124,7 +124,7 @@ const Login = ({showWelcomeHandler}) => {
           const vendorId = data.vendorId
           console.log("checking for VendorId:",vendorId)
           const vendorResponse = await fetch(`${API_URL}/vendor/single-vendor/${vendorId}`)
-          //window.location.reload()
+          window.location.reload()
           const vendorData = await vendorResponse.json();
           if(vendorResponse.ok){
             const vendorFirmId = vendorData.vendorFirmId;
